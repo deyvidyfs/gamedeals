6dc0fb908d059563f19030a6be0fcf96aa89fee5
Deyvidy F.S <deyvidyfs@gmail.com>
2021-06-17 15:24:54 -0300

    initializing project

---
+++ gameDealsApp/gameDealsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  gameDealsApp
+//
+//  Created by Deyvidy Luã F.S on 17/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
